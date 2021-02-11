835dbe40946c8a9547f4db297d018fbe3cce0615
Renan Proença <renan.maringolo@gmail.com>
2021-02-11 15:44:14 -0300

    tirei a camada de app record por nao fazer muito sentido agora

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,8 +1,25 @@
-class State < ApplicationRecord
+require 'sqlite3'
+
+class State
   attr_reader :id, :uf, :state_name
   def initialize(id: nil, uf:, state_name:)
     @id = id
     @uf = uf
     @state_name = state_name
+  end
+
+  def self.all
+    db = SQLite3::Database.open "db/database.db"
+    db.results_as_hash = true
+    states = db.execute "SELECT id, uf, state_name FROM states"
+    db.close
+    states.map {|state| new(id: state['id'], uf: state['uf'], state_name: state['state_name'])}
+  end
+
+  def save_to_db
+    db = SQLite3::Database.open "db/database.db"
+    db.execute "INSERT INTO states (uf, state_name) VALUES('#{uf}', '#{state_name}')"
+    db.close
+    self
   end
 end

17bf1c6834854f7744c43f6d05e4ffef946c4464
Renan Proença <renan.maringolo@gmail.com>
2021-02-13 18:12:29 -0300

    refactoring to File reader

--- app/services/states_api.rb
+++ app/services/states_api.rb
@@ -1,34 +1,12 @@
 class StatesAPI
-  def self.configure_driver(driver_type)
-    @driver_type = driver_type
-  end
-
-  def self.driver
-    @driver_type
-  end
-
   def self.get_state(id)
-    if driver == :http_api
-      api_response =
-        RestClient
-        .get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/#{id}")
-        .body
-    elsif driver == :file
-      api_response = File.read("spec/fixtures/states/#{id}.json")
-    end
+    api_response = File.read("spec/fixtures/states/#{id}.json")
     
     JSON.parse(api_response)
   end
 
   def self.list_states
-    if driver == :http_api
-      api_response =
-        RestClient
-        .get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/")
-        .body
-    elsif driver == :file
-      api_response = File.read("spec/fixtures/states.json")
-    end
+    api_response = File.read("spec/fixtures/states.json")
 
     JSON.parse(api_response)
   end
